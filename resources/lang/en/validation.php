9dedfa3325bec0e2af850bde1c1157161b176980
Syafwan000 <muhammadsyafwan04@gmail.com>
2022-01-10 16:37:34 +0700

    change: validation lang

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -105,7 +105,7 @@ return [
     'prohibited_unless' => 'The :attribute field is prohibited unless :other is in :values.',
     'prohibits' => 'The :attribute field prohibits :other from being present.',
     'regex' => 'The :attribute format is invalid.',
-    'required' => 'The :attribute field is required.',
+    'required' => 'Kolom :attribute wajib diisi',
     'required_if' => 'The :attribute field is required when :other is :value.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
     'required_with' => 'The :attribute field is required when :values is present.',
